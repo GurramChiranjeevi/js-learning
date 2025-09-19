1c08a13ddac8cb722a47c6ffc93b70ebaee0c814
GurramChiranjeevi <gurramchiranjeevi2006@gmail.com>
2025-09-19 14:27:37 +0530

    Update sumOfDigits.js

--- learning-approach/sumOfDigits.js
+++ learning-approach/sumOfDigits.js
@@ -1,3 +1,26 @@
-const lastDigit = 1;
-const sumOfAll = lastDigit;
-console.log("sum of until",lastDigit," is",sumOfAll);
+let lastDigit = 1;
+if(lastDigit === 1){
+
+  const sumOfAll = lastDigit ;
+  console.log("sum of until",lastDigit," is",sumOfAll);
+}
+if(lastDigit === 2){
+
+  const sumOfAll = lastDigit + lastDigit -1;
+  console.log("sum of until",lastDigit," is",sumOfAll);
+}
+if(lastDigit === 3){
+
+  const sumOfAll = lastDigit + lastDigit -1 + lastDigit -2;
+  console.log("sum of until",lastDigit," is",sumOfAll);
+}
+if(lastDigit === 4){
+
+  const sumOfAll = lastDigit + lastDigit -1 + lastDigit -2 + lastDigit -3;
+  console.log("sum of until",lastDigit," is",sumOfAll);
+}
+if(lastDigit === 5){
+
+  const sumOfAll = lastDigit + lastDigit -1 + lastDigit -2 + lastDigit -3 + lastDigit -4;
+  console.log("sum of until",lastDigit," is",sumOfAll);
+}
