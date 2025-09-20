320df750f156d4bd5f17b1284ac6cf84a4f8e77a
GurramChiranjeevi <gurramchiranjeevi2006@gmail.com>
2025-09-20 16:07:38 +0530

    Updated fibonacci.js for some expected inputs/outputs

--- learning-approach/fibonacci.js
+++ learning-approach/fibonacci.js
@@ -1,6 +1,133 @@
-const noOfFibonacciNumbers = 1;
-const fibonacciSeries = 0;
-console.log("fibonacci for", noOfFibonacciNumbers, "is",fibonacciSeries);
+const noOfFibonacciTerms = 7;
+let firstTerm = 0;
+let secondTerm = 1;
+let fibonacciSeries;
+if ( noOfFibonacciTerms === 1) {
+  fibonacciSeries = firstTerm ;
+  console.log("fibonacci for", noOfFibonacciTerms, "is :",fibonacciSeries);
 
+}
+if ( noOfFibonacciTerms === 2){
+
+  fibonacciSeries = firstTerm;
+  console.log("fibonacci for", noOfFibonacciTerms, "is :",fibonacciSeries);
+  fibonacciSeries = secondTerm ;
+  console.log(fibonacciSeries)
+  
+}
+if ( noOfFibonacciTerms === 3){
+  fibonacciSeries = firstTerm ;
+  console.log("fibonacci for", noOfFibonacciTerms, "is :",fibonacciSeries);
+  fibonacciSeries = secondTerm ;
+  console.log(fibonacciSeries);
+  fibonacciSeries = firstTerm + secondTerm ;
+  console.log(fibonacciSeries);
+  
+
+}
+
+if ( noOfFibonacciTerms === 4){
+  fibonacciSeries = firstTerm ;
+  console.log("fibonacci for", noOfFibonacciTerms, "is :",fibonacciSeries);
+
+  fibonacciSeries = secondTerm ;
+  console.log(fibonacciSeries);
+  
+  fibonacciSeries = firstTerm + secondTerm ;
+  console.log(fibonacciSeries);
+  firstTerm = secondTerm;
+  secondTerm = fibonacciSeries;
+
+  fibonacciSeries = firstTerm + secondTerm ;
+  console.log(fibonacciSeries);
+  
+  
+
+}
+
+if ( noOfFibonacciTerms === 5){
+  fibonacciSeries = firstTerm ;
+  console.log("fibonacci for", noOfFibonacciTerms, "is :",fibonacciSeries);
+
+  fibonacciSeries = secondTerm ;
+  console.log(fibonacciSeries);
+  
+  fibonacciSeries = firstTerm + secondTerm ;
+  console.log(fibonacciSeries);
+  firstTerm = secondTerm;
+  secondTerm = fibonacciSeries;
+
+  fibonacciSeries = firstTerm + secondTerm ;
+  console.log(fibonacciSeries);
+  firstTerm = secondTerm ;
+  secondTerm = fibonacciSeries ;
+
+  fibonacciSeries = firstTerm + secondTerm ;
+  console.log(fibonacciSeries);
+
+  
+
+}
+
+if ( noOfFibonacciTerms === 6){
+  fibonacciSeries = firstTerm ;
+  console.log("fibonacci for", noOfFibonacciTerms, "is :",fibonacciSeries);
+
+  fibonacciSeries = secondTerm ;
+  console.log(fibonacciSeries);
+  
+  fibonacciSeries = firstTerm + secondTerm ;
+  console.log(fibonacciSeries);
+  firstTerm = secondTerm;
+  secondTerm = fibonacciSeries;
+
+  fibonacciSeries = firstTerm + secondTerm ;
+  console.log(fibonacciSeries);
+  firstTerm = secondTerm ;
+  secondTerm = fibonacciSeries ;
+
+  fibonacciSeries = firstTerm + secondTerm ;
+  console.log(fibonacciSeries);
+  firstTerm = secondTerm ;
+  secondTerm = fibonacciSeries ;
+
+  fibonacciSeries = firstTerm + secondTerm ;
+  console.log(fibonacciSeries);
+    
+
+}
+
+if ( noOfFibonacciTerms === 7){
+  fibonacciSeries = firstTerm ;
+  console.log("fibonacci for", noOfFibonacciTerms, "is :",fibonacciSeries);
+
+  fibonacciSeries = secondTerm ;
+  console.log(fibonacciSeries);
+  
+  fibonacciSeries = firstTerm + secondTerm ;
+  console.log(fibonacciSeries);
+  firstTerm = secondTerm;
+  secondTerm = fibonacciSeries;
+
+  fibonacciSeries = firstTerm + secondTerm ;
+  console.log(fibonacciSeries);
+  firstTerm = secondTerm ;
+  secondTerm = fibonacciSeries ;
+
+  fibonacciSeries = firstTerm + secondTerm ;
+  console.log(fibonacciSeries);
+  firstTerm = secondTerm ;
+  secondTerm = fibonacciSeries ;
+
+  fibonacciSeries = firstTerm + secondTerm ;
+  console.log(fibonacciSeries);
+  firstTerm = secondTerm ;
+  secondTerm = fibonacciSeries ;
+
+  fibonacciSeries = firstTerm + secondTerm ;
+  console.log(fibonacciSeries);
+  
+
+}
 
 
