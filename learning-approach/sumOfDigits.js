376ba6efdd84533f918ddc5fad7b7912d9902455
GurramChiranjeevi <gurramchiranjeevi2006@gmail.com>
2025-09-20 11:09:43 +0530

    Update sumOfDigits.js with loop

--- learning-approach/sumOfDigits.js
+++ learning-approach/sumOfDigits.js
@@ -3,11 +3,11 @@ let sumOfAll = 0;
 let oneLess ;
 
 oneLess = lastDigit ;
-  while( true) {
-    sumOfAll = sumOfAll + oneLess;
-    oneLess -- ; 
-    if ( oneLess === 0){
-      break;
-    }
+while( true) {
+  sumOfAll = sumOfAll + oneLess;
+  oneLess -- ; 
+  if ( oneLess === 0){
+    break;
   }
-  console.log(" sum of until ",lastDigit, " is ", sumOfAll);
+}
+console.log(" sum of until ",lastDigit, " is ", sumOfAll);
