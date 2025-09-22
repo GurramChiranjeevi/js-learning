a749fce4958753e8b65940d901343ee892c2fd53
GurramChiranjeevi <gurramchiranjeevi2006@gmail.com>
2025-09-22 09:59:03 +0530

    Update stringLength.js using while loop

--- learning-approach/stringLength.js
+++ learning-approach/stringLength.js
@@ -1,3 +1,9 @@
-const givenString = "hello";
-const givenStringLength = 0;
+while(true) {
+  lengthCounter = lengthCounter + 1;
+  if(givenString[lengthCounter] === undefined) {
+    break;
+  }
+
+}
+givenStringLength = lengthCounter ;
 console.log("length of ",givenString," is",givenStringLength);
