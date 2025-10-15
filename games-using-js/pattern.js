d0e077af05d93abca642dd2467ce4e1170997cd8
GurramChiranjeevi <gurramchiranjeevi2006@gmail.com>
2025-10-15 12:26:07 +0530

    Update pattern.js

--- games-using-js/pattern.js
+++ games-using-js/pattern.js
@@ -1,23 +1,47 @@
-//we can print circle, parabola, chess pattern
 function sqr(x) {
   return x * x;
 }
 
 function getTileIndex(style, x , y, h, k, r){
   if(style === "chess") {
-    return ((x + y) % 2);
+    return (x + y) % 2;  
   }
   if(style === "circle") {
-    return (Math.sqrt(sqr(x - h) + sqr(y - k)) <= r) ? 0 : 1;
+    return (Math.sqrt(sqr(x - h) + sqr(y - k)) === r) ? 0 : 1;
+  }
+  if(style === "concentric-circle") {
+    const d = Math.sqrt(sqr(x - h) + sqr(y - k));
+    if(d >= r) {
+      return 0;
+    }
+    if(d < 5) {
+      return 1;
+    }
+    if(d < 10) {
+      return 2;
+    }
+
+    if(d < 15) {
+      return 3;
+    }
+    if(d < 20) {
+      return 4;
+    }
+    return 1;
   }
   if(style === "parabola") {
-    return (sqr(x - h) <= (4 * 1) * (y - k)) ? 1 : 0;
+    return (sqr(y - h) <= (4 * 1) * (x - k)) ? 1 : 0;
   }
 
 }
 
 function getTile(style, row, col, h, k, r){
   return TILE_PATTERNS[getTileIndex(style, row, col, h, k, r)]
+}
+
+function delay(n) {
+  for(let i = 0; i < n * 1000000000; i++) { 
+  }
 }
 
 const TILE_PATTERNS = ["⬜️", "⬛️", "🟩", "🟫", "🟧"];
@@ -33,15 +57,18 @@ function pattern(style,rows, cols, h, k, r) {
     }
     lines.push(line);
   }
-  return lines.join('\n');
+  return lines;
 }
 
 function main() {
-  const height = 30;
-  const width = 30;
-  const h = 15;
-  const k = 15;
-  const r = 13;
-  console.log(pattern("parabola",height, width, h, k, r));
+  const height = 100;
+  const width = 100;
+  const h = 50;
+  const k = 50;
+  const r = 25;
+  const drawing = pattern("concentric-circle",height, width, h, k, r);
+  for(let i = 0; i < drawing.length; i++) {
+    console.log(drawing[i]);
+  }
 }
 main();
